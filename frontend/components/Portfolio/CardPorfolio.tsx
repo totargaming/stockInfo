970fef90c452cf58815bacdcc46ee4baa740472c
totargaming <totargaming@gmail.com>
2025-02-25 21:39:31 +1030

    refactor Card and CardPortfolio components; update links to improve navigation and consistency

--- frontend/components/Portfolio/CardPorfolio.tsx
+++ frontend/components/Portfolio/CardPorfolio.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 interface Props {
@@ -8,7 +9,12 @@ interface Props {
 const CardPortfolio = ({ symbol, onDelete }: Props) => {
   return (
     <div className="flex items-center justify-between w-full p-2 mb-2 bg-white dark:bg-gray-700 rounded-lg shadow-md">
-      <span className="text-gray-700 dark:text-gray-300">{symbol}</span>
+      <Link
+        href={`/company/${symbol}`}
+        className="text-gray-700 dark:text-gray-300"
+      >
+        {symbol}
+      </Link>
       <button
         onClick={() => onDelete(symbol)}
         className="p-1 text-red-500 hover:text-red-700 focus:outline-none"
