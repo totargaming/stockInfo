80e60cdb8482b0927d80b2bcf14185abcde43783
totargaming <totargaming@gmail.com>
2025-02-27 19:04:58 +1030

    simplify error handling in searchCompanies API; log error directly for improved clarity

--- frontend/app/api/api.tsx
+++ frontend/app/api/api.tsx
@@ -12,16 +12,8 @@ export const searchCompanies = async (query: string) => {
     );
     return data;
   } catch (error) {
-    if (axios.isAxiosError(error)) {
-      console.log("Axios error message: ", error.message);
-      return [];
-    } else if (error instanceof Error) {
-      console.log("General error message: ", error.message);
-      return [];
-    } else {
-      console.log("An unexpected error occurred");
-      return [];
-    }
+    console.log(error);
+    return [];
   }
 };
 
